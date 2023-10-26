c1ba4f5ddeaeaf2ad827fbacc656eaded6249ab7
Jasmine Hernandez <bezierbee@gmail.com>
2023-10-25 20:17:04 -0400

    adding js and py files for double greetings pluralize labs

---
+++ double.py
@@ -0,0 +1,6 @@
+def double(n):
+    return n * 2
+userstring = input("Number please: ")
+usernum = int(userstring) #in py we use "int" inplace of parseInt to change integer to string
+
+print(double(usernum))
---
+++ greetings.py
@@ -0,0 +1,6 @@
+#program a function that prints hello 5 times
+def greet():
+    print("hello")
+#now create a for loop to duplicate 5 times()
+for i in range(0, 5, 1):
+    greet()
