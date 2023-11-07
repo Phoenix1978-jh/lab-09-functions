192ada478fa9ae89839b13d46f816e9bd6373624
jasmine hernandez <jasminehernandez@jasmines-imac.myfiosgateway.com>
2023-11-06 22:58:02 -0500

    uploading missing file

---
+++ factorial.py
@@ -0,0 +1,12 @@
+
+userNum = input("Number please: ")
+userstring = int(userNum) # new variable to convert string (userNum) into a number*/
+
+
+def factorial(n):
+    factorNum = 1
+    if n >= 1:
+        for i in range(1, userstring +1):
+            factorNum = factorNum * 1
+
+print(factorial(userstring))
